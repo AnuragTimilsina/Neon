8ee389b1d4fce4818a0233743a704530a6d3685f
Gnik Droy <gnikdroy@gmail.com>
2021-01-21 15:59:05 +0000

    add oscar-rest-api

---
+++ shop/urls.py
@@ -0,0 +1,14 @@
+from django.apps import apps
+from django.urls import include, path
+from django.contrib import admin
+
+urlpatterns = [
+    path('i18n/', include('django.conf.urls.i18n')),
+
+    # The Django admin is not officially supported; expect breakage.
+    # Nonetheless, it's often useful for debugging.
+
+    path('admin/', admin.site.urls),
+    path("api/", include("oscarapi.urls")),
+    path('', include(apps.get_app_config('oscar').urls[0])),
+]
